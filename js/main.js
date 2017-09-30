4679ab05cd3a608a91fc9b06b237a10d45a1a5f2
tpielaszkiewicz <tpielaszkiewicz@gmail.com>
2017-09-30 13:58:50 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,134 @@
+$(document).ready(function () {
+    pageInit();
+    passIn();
+    plotterSlider();
+    
+//    changeNavbar();
+//    changeSizeHandler();
+    //    rotateIcon();
+    smoothScroll();
+});
+
+function pageInit() {
+    if ($(window).scrollTop() > 150) {
+        $('.row').css('left', '0%');
+    }
+}
+
+function plotterSlider(){
+    $('#plotter').on('click', function() {
+        $('#slider1').removeClass('slider-invisible');
+    });
+    
+    $('#exit').on('click', function() {
+        $('#slider1').addClass('slider-invisible');
+    })
+}
+
+
+function passIn(){
+    $(window).on('scroll', function(){
+        
+        if ($(window).scrollTop() > 130) {
+            $('.row:nth-child(1)').css('left','0%');
+        }
+        
+        if ($(window).scrollTop() > 200) {
+            $('.row:nth-child(2)').css('left','0%');
+        }
+        
+        if ($(window).scrollTop() > 450) {
+            $('.row:nth-child(3)').css('left','0%');
+        }
+        
+        if ($(window).scrollTop() > 900) {
+            $('.row:nth-child(4)').css('left','0%');
+        }
+        
+        if ($(window).scrollTop() > 1000) {
+            $('.row:nth-child(5)').css('left','0%');
+        }
+    })
+}
+
+function changeNavbar() {
+    $(window).scroll(function () {
+        if ($(window).outerWidth() >= 768) {
+
+            if ($(window).scrollTop() > 10) {
+                $('.navbar-default').addClass('scrolled-menu');
+                $('#main-nav a').addClass('scrolled-link');
+
+            } else {
+                $('.navbar-default').removeClass('scrolled-menu');
+                $('#main-nav a').removeClass('scrolled-link');
+            }
+        }
+    })
+}
+
+function changeSizeHandler() {
+
+    $(window).resize(function () {
+        if ($(window).outerWidth() < 768) {
+            $('.navbar-default').removeClass('scrolled-menu');
+            $('#main-nav a').removeClass('scrolled-link');
+        } else if ($(window).scrollTop() > 10) {
+            $('.navbar-default').addClass('scrolled-menu');
+            $('#main-nav a').addClass('scrolled-link');
+        }
+    })
+
+}
+
+function rotateIcon() {
+    $('.service-icon').on({
+        'mouseenter': function () {
+            $(this).css('transform', 'rotate(360deg)');
+        },
+        'mouseleave': function () {
+            $(this).css('transform', 'rotate(0deg)');
+        }
+    })
+}
+
+function smoothScroll() {
+    $("nav a").on('click', function (event) {
+
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+            console.log(hash);
+            console.log($(hash).offset().top);
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+};
+
+// metoda symulująca wysłanie maila - metoda post ajax - po stronie serwera juz te dane powinny byc obronione i mail wysłany
+function sendMail() {
+    $.ajax('http://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        data: {
+            title: 'fssoo',
+            body: 'bsssar',
+            userId: 123
+        }
+    }).then(function (data) {
+        console.log(data);
+   });
+}
